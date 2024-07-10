d7c13c47d1dc0925656edb17740a0b1c8c313c75
macplanck <chienshuitingveryshui@gmail.com>
2024-07-11 00:00:32 +0800

    7th commit

--- Lecture_02/Example01/01_RTL/01_run.sh
+++ Lecture_02/Example01/01_RTL/01_run.sh
@@ -1,25 +1,3 @@
-# ------------------------------------------------
-#   Version 5
-# ------------------------------------------------
-
-# if [ -f '../02_REPORT/RESULT.vvp' ]; then
-#     rm '../02_REPORT/RESULT.vvp'
-# fi
-
-# if [ -f '../02_REPORT/WAVEFORM.vcd' ]; then
-#     rm '../02_REPORT/WAVEFORM.vcd'
-# fi
-
-# iverilog -g 2012 -o ../02_REPORT/RESULT.vvp ../00_TESTBED/TESTBED.sv 
-
-# if [ -f '../02_REPORT/RESULT.vvp' ]; then
-#     vvp ../02_REPORT/RESULT.vvp
-# fi
-
-# ------------------------------------------------
-#   Version 6
-# ------------------------------------------------
-
 # Initialize commands and log file location
 log_file="./../02_REPORT/syn.log"
 
